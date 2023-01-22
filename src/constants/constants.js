0e0a9c59c162427cd82e8f2ca02ee2960b836e34
pranjaypathak411 <pranjaypathak411@gmail.com>
2023-01-22 23:58:50 +0530

    Add constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,8 @@
+export const ERROR = {
+    INVALID_INPUT: 'Invalid input',
+    INVALID_AMOUNT: 'Invalid amount',
+    DUPLICATE_ADDRESS: 'Duplicate address'
+}
+
+export const DIVIDERS = [' ', ',', '=']
+
